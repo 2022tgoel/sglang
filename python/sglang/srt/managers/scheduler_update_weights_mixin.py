d8fd1cc4b4be95f4efae477907393938b0c5c308
Tarushii Goel <tarushii@thinkingmachines.ai>
2026-04-04 02:31:50 +0000

    fixes

--- python/sglang/srt/managers/scheduler_update_weights_mixin.py
+++ python/sglang/srt/managers/scheduler_update_weights_mixin.py
@@ -95,10 +95,12 @@ class SchedulerUpdateWeightsMixin:
         else:
             worker = self.draft_worker or self.tp_worker
         success, message = worker.update_weights_from_tensor(recv_req)
-        if recv_req.flush_cache:
-            flush_cache_success = self.flush_cache()
-            assert flush_cache_success, "Cache flush failed after updating weights"
-        if not success:
+        # TODO extract common code b/t update_weights_from_distributed and update_weights_from_tensor later
+        if success:
+            if recv_req.flush_cache:
+                flush_cache_success = self.flush_cache()
+                assert flush_cache_success, "Cache flush failed after updating weights"
+        else:
             logger.error(message)
         torch.distributed.barrier(group=self.tp_cpu_group)
         return UpdateWeightsFromTensorReqOutput(success, message)
